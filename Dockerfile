395e382a3993a37a7e721e84bf9ac120a0a3f55f
Nathan Grubb <nate@nathangrubb.io>
2020-12-23 15:40:52 +0700

    General Improvements (#1)
    * Improve error responses for improved debugging
    * Add support for a caching via a Redis server
    * Remove compiled go binary
    * Switch to go modules

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM golang:latest 
+FROM golang:1.15.6
 MAINTAINER Nathan Grubb "me@nathangrubb.io"
 
 RUN mkdir /service
-RUN go get github.com/gorilla/mux
 
-ADD . /service/ 
+ADD go.mod /service/
+ADD go.sum /service/
 WORKDIR /service
+RUN go mod download
+
+ADD *.go /service/
 RUN go build -o main .
 
 ENTRYPOINT ["/service/main"]
--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-  "bufio"
+	"strconv"
 	"strings"
+	"time"
 )
+
+type FreeCurrConvErrorResponse struct {
+	Status int    `json:"status"`
+	Error  string `json:"error"`
+}
 
 type ExchangeRate struct {
 	From string
@@ -19,34 +28,56 @@ type ExchangeRate struct {
 	Rate float64
 }
 
+type ErrorResponse struct {
+	Error string
+}
+
 type RestService struct {
 	FreeCurrConvApiKey string
+	RedisServer        string
+	RedisCacheDuration time.Duration
 }
 
 var service *RestService
 
 func main() {
 	apiKeyFilename := os.Getenv("FREE_CURRCONV_API_KEY_FILE")
-  apiKeyFile, err := os.Open(apiKeyFilename)
-  defer apiKeyFile.Close()
+	redisServerAndPort := os.Getenv("REDIS_SERVER_AND_PORT")
+	redisCacheDurationInMinutes := os.Getenv("REDIS_CACHE_DURATION_IN_MINUTES")
+	apiKeyFile, err := os.Open(apiKeyFilename)
+	defer apiKeyFile.Close()
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  reader := bufio.NewReader(apiKeyFile)
-  apiKey, moreToRead, err := reader.ReadLine()
+	reader := bufio.NewReader(apiKeyFile)
+	apiKey, moreToRead, err := reader.ReadLine()
 
-  if err != nil {
-    fmt.Println("Can't read file (%s) where api key should be stored", apiKeyFilename)
-    panic(err)
-  }
+	if err != nil {
+		fmt.Println("Can't read file (%s) where api key should be stored", apiKeyFilename)
+		panic(err)
+	}
 
-  if moreToRead {
-    panic("API key is longer than expected")
-  }
+	if moreToRead {
+		panic("API key is longer than expected")
+	}
 
-	service = &RestService{ FreeCurrConvApiKey: string(apiKey) }
+	minutes, err := strconv.ParseUint(redisCacheDurationInMinutes, 10, 32)
+
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Defaulting to caching for 15 minutes in Redis")
+		minutes = 15
+	}
+
+	service = &RestService{
+		FreeCurrConvApiKey: string(apiKey),
+		RedisServer:        redisServerAndPort,
+		RedisCacheDuration: time.Duration(minutes) * time.Minute,
+	}
+
+	fmt.Printf("%+v\n", service)
 	service.startService()
 }
 
@@ -64,6 +95,23 @@ func GetCurrency(responseWriter http.ResponseWriter, requestReader *http.Request
 	toCurrency := strings.ToUpper(params["to"])
 	currencyQuery := fmt.Sprintf("%s_%s", fromCurrency, toCurrency)
 
+	cachedValue, err := getCachedCurrencyValue(currencyQuery)
+	if err == nil {
+		fmt.Printf("Api=GetCurrency Action=UsingRedisCache Query=%s\n", currencyQuery)
+		rate := &ExchangeRate{
+			From: fromCurrency,
+			To:   toCurrency,
+			Rate: cachedValue,
+		}
+
+		if err := writeJson(responseWriter, rate); err != nil {
+			responseWriter.WriteHeader(http.StatusInternalServerError)
+			fmt.Println(err)
+		}
+		return
+	}
+
+	fmt.Printf("Api=GetCurrency Action=UsingCurrConv Query=%s\n", currencyQuery)
 	url := url.URL{
 		Scheme: "https",
 		Host:   "free.currconv.com",
@@ -76,52 +124,111 @@ func GetCurrency(responseWriter http.ResponseWriter, requestReader *http.Request
 	queryString.Set("apiKey", service.FreeCurrConvApiKey)
 	url.RawQuery = queryString.Encode()
 
+	responseWriter.Header().Set("Content-Type", "application/json")
+
 	rsp, err := http.Get(url.String())
 	if err != nil {
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
+		writeError(responseWriter, err.Error())
 		return
 	}
 
 	defer rsp.Body.Close()
 	rspBody, err := ioutil.ReadAll(rsp.Body)
+
 	if err != nil {
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
+		writeError(responseWriter, err.Error())
+		return
+	}
+
+	//API returns either
+	//200: {"<from_currency>_<to_currency>":<rate>}
+	//400: {"status":<status_code>,"error":"error_message"}
+	if rsp.StatusCode != http.StatusOK {
+		var errorResponse FreeCurrConvErrorResponse
+
+		if err = json.Unmarshal(rspBody, &errorResponse); err != nil {
+			//Unknown response format
+			writeError(responseWriter, err.Error())
+			fmt.Println(strings.TrimSuffix(string(rspBody), "\n"))
+			return
+		}
+
+		writeError(responseWriter, errorResponse.Error)
 		return
 	}
 
 	var result map[string]float64
 	if err = json.Unmarshal(rspBody, &result); err != nil {
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
-		fmt.Print(strings.TrimSuffix(string(rspBody), "\n"))
+		writeError(responseWriter, err.Error())
+		fmt.Println(strings.TrimSuffix(string(rspBody), "\n"))
 		return
 	}
 
 	if _, ok := result[currencyQuery]; !ok {
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(fmt.Sprintf("%s not present in JSON result object", currencyQuery))
+		writeError(responseWriter, fmt.Sprintf("%s not present in JSON result object", currencyQuery))
 		return
 	}
 
-	rate := &ExchangeRate{
+	var rate float64 = result[currencyQuery]
+
+	if err := setCachedCurrencyValue(currencyQuery, rate); err != nil {
+		fmt.Println(err)
+	}
+
+	rateResponse := &ExchangeRate{
 		From: fromCurrency,
 		To:   toCurrency,
-		Rate: result[currencyQuery],
+		Rate: rate,
 	}
 
-	jsonData, err := json.Marshal(rate)
+	if err := writeJson(responseWriter, rateResponse); err != nil {
+		responseWriter.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+	}
+}
+
+func writeError(responseWriter http.ResponseWriter, errorString string) {
+	responseWriter.WriteHeader(http.StatusInternalServerError)
+
+	error := &ErrorResponse{
+		Error: errorString,
+	}
+
+	if err := writeJson(responseWriter, error); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func writeJson(responseWriter http.ResponseWriter, data interface{}) error {
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	responseWriter.Header().Set("Content-Type", "application/json")
 	if _, err = responseWriter.Write(jsonData); err != nil {
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
-		return
+		return err
 	}
+
+	return nil
+}
+
+func getCachedCurrencyValue(key string) (float64, error) {
+	stringValue, err := getRedisServer().Get(context.Background(), key).Result()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return strconv.ParseFloat(stringValue, 64)
+}
+
+func setCachedCurrencyValue(key string, value float64) error {
+	_, err := getRedisServer().Set(context.Background(), key, value, service.RedisCacheDuration).Result()
+	return err
+}
+
+func getRedisServer() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr: service.RedisServer,
+	})
 }
